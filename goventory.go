5c403b2f72e68f6b17d19724553277f5a7be7c54
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-11-09 22:07:48 +0500

    Update

---
+++ goventory.go
@@ -0,0 +1,22 @@
+/**
+ *  Appzaib  (https://appzaib.com)
+ *
+ *
+ *  @copyright      2019-present Appzaib          <info@appzaib.com>
+ *                  2019-present Rana Jahanzaib   <iam@ranajahanzaib.com>
+ *
+ *
+ *  @author         Rana Jahanzaib  <iam@ranajahanzaib.com>
+ *
+ *  @docs           https://docs.appzaib.com/goventory/goventory
+ */
+
+package goventory
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Just getting started with Goventory!")
+}
